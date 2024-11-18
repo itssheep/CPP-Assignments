cd18fc1088e53a0b84a230cd1cb800424f835332
Aaron Crane <acrane23@mun.ca>
2024-11-17 21:08:03 -0330

    Update Histogram.hpp

--- A3/Histogram.hpp
+++ A3/Histogram.hpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 
+using namespace std;
+
+class Histogram
+{
+	public:
+		Histogram(std::vector<double> endpoints);
+
+		// Copy constructor
+		Histogram(const Histogram& other);
+
+		// Move constructor
+		Histogram(Histogram&& other);
+
+		// Destructor
+		~Histogram() {clear();}
+
+		// Clear Histogram Statistics and Data
+		void clear();
+
+		// Add a new value to the histogram
+		bool update(double newvalue);
+
+		// Copy assignment operator
+		Histogram& operator=(Histogram& other);
+
+		// Move assignment operator
+		Histogram& operator=(Histogram&& other);
+
+		// Overloaded addition operator
+		Histogram operator+(Histogram other);
+
+		void display(ostream& os)
+		{
+		        if (endpoints.size() > 0)
+		        {
+		                cout << setfill(' ') << right << fixed << showpoint << setprecision(2);
+		                for (int i = 0; i < endpoints.size()-1; i++)
+		                {
+		                        os << "[" << setw(5) << right << endpoints[i] << ", " << setw(5) << left << endpoints[i+1] << ")" << "\t";
+		                        for (int j = 0; j < numberinbin[i]; j++)
+		                        {
+		                                os << "*";
+		                        }
+		                        os << endl;
+		                }
+		        }
+		}
+
+	private:
+		vector<double> endpoints;
+		vector<double> values;
+		vector<int> numberinbin;
+};
